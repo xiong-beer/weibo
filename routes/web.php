e464f282627b860aa515119c26b470cf2bf19573
xiong-beer <xiongbeer@live.cn>
2020-02-21 21:41:32 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
+
+
+
+
+
+
